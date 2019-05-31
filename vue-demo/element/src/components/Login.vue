407d51af16eebeff3c4f4be897446fe8f7d0c40c
unknown <2670236233@qq.com>
2019-05-31 14:23:57 +0800

    tt

--- vue-demo/element/src/components/Login.vue
+++ vue-demo/element/src/components/Login.vue
@@ -2,10 +2,10 @@
     <div class="login">
         <div class="container">
             <p class="title">后台管理系统</p>
-            <div class="msg">
+            <div class="msg mt20">
               <el-input v-model="user" placeholder="请输入用户名"></el-input>
             </div>
-            <div class="msg">
+            <div class="msg mt20">
               <el-input v-model="password" placeholder="请输入密码"></el-input>
             </div>
             <div class="btn mt20">
@@ -28,6 +28,12 @@ export default {
     login () {
       if (this.user && this.password) {
         localStorage.setItem('userName', this.user)
+        // 字符串
+        // this.$router.push('/main')
+        // 对象
+        // this.$router.push({path: '/main'})
+        // 编程式跳转
+        this.$router.push({name: 'main', params: {userId: 1}})
       }
     }
   }
@@ -44,6 +50,7 @@ export default {
   margin: 0 auto;
   box-shadow: 0 0 20px #cac6c6;
   padding: 50px;
+  margin-top: 200px;
 }
 .title{
   font-size: 26px;
