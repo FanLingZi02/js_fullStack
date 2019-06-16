ff064255eafa831a3e4bee9a71caafd046db7e13
unknown <2670236233@qq.com>
2019-06-16 14:07:51 +0800

    tt

---
+++ VUE/vuex-counter/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{$store.state.count}} times,
+    <button @click="increment">+</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    increment () {
+      this.$emit('increment')
+    } 
+  }
+}
+</script>
+
+<style>
+</style>
